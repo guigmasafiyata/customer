7be8dac73db8bc1b7b5062d75de2759ec7baba87
GUIGMA Safiyata <GUIGMA Safiyata@Ordinateur>
2024-08-14 15:07:02 +0200

    Etat initial Ajout des classes entités Ajout MustomerManager

---
+++ src/main/java/com/mycompany/customer/DiscountConverter.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.customer;
+
+import com.mycompany.customer.entity.Discount;
+import jakarta.faces.component.UIComponent;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.convert.Converter;
+import jakarta.faces.convert.FacesConverter;
+import jakarta.inject.Inject;
+
+/**
+ *
+ * @author GUIGMA Safiyata
+ */
+@FacesConverter(value = "discountConverter", managed=true)
+public class DiscountConverter implements Converter<Discount> {
+     @Inject
+  private DiscountManager discountManager;
+
+  /**
+   * Convertit une String en Discount.
+   *
+   * @param value valeur à convertir
+   */
+  public Discount getAsObject(FacesContext context, UIComponent component, String value) {
+    if (value == null) return null;
+    return discountManager.findById(value);
+  }
+
+  /**
+   * Convertit un Discount en String.
+   *
+   * @param value valeur à convertir
+   */
+  public String getAsString(FacesContext context, UIComponent component, Discount discount) {
+    if (discount == null) return "";
+    return discount.getCode();
+  }
+}
+    
